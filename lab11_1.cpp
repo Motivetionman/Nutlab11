3778dc0fddf8027a071c223acc0a01f93db84f7c
Motivationman_74 <120492095+Motivetionman@users.noreply.github.com>
2023-12-24 22:02:12 +0700

    Update lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,23 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    for (int i = 0; i < 3; ++i) {
+        cin.get(); 
+    }
+
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    int index = rand() % 9;
+
+    cout << "You will get"<< grades[index]<<"in this 261102."<< endl;
+
+    return 0;
+}
